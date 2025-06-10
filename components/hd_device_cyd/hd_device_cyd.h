95140a583c807d4f5d0355b33990b30bbc628d4f
samsamxu <50017914+samsamxu@users.noreply.github.com>
2025-06-10 16:26:30 +0800

    Update hd_device_cyd.h

--- components/hd_device_cyd/hd_device_cyd.h
+++ components/hd_device_cyd/hd_device_cyd.h
@@ -5,40 +5,26 @@
 #include "esphome/core/log.h"
 #include "LGFX.h"
 #include "lvgl.h"
-#include "esphome/components/cst816/touchscreen/cst816_touchscreen.h"  // 使用ESPHome自带的CST816驱动
-
-// CST816触摸屏引脚定义
-#define TOUCH_SDA 21    // I2C SDA引脚
-#define TOUCH_SCL 22    // I2C SCL引脚
-#define TOUCH_RST -1    // 复位引脚（-1表示不使用）
-#define TOUCH_INT -1    // 中断引脚（-1表示不使用）
-#define TOUCH_ROTATION 0 // 触摸旋转角度 (0, 90, 180, 270)
 
 namespace esphome {
 namespace hd_device {
 
-static const char *const TAG = "HD_DEVICE";
+class HaDeckDevice : public Component {
+ public:
+  void setup() override;
+  void loop() override;
+  float get_setup_priority() const override;
 
-// 前向声明类
-class HaDeckDevice;
+ private:
+  unsigned long time_ = 0;
+  LGFX lcd;
+  lv_disp_draw_buf_t draw_buf;
+  lv_color_t *buf;
+  lv_group_t *group;
+  uint8_t brightness_ = 255;
 
-// 声明全局设备指针
-extern HaDeckDevice *global_device;
-
-class HaDeckDevice : public Component
-{
-public:
-    void setup() override;
-    void loop() override;
-    float get_setup_priority() const override;
-    uint8_t get_brightness();
-    void set_brightness(uint8_t value);
-    
-    cst816::CST816Touchscreen touch_;  // 使用正确的类型名 CST816S
-    
-private:
-    unsigned long time_ = 0;
-    uint8_t brightness_ = 0;
+  static void IRAM_ATTR flush_pixels(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p);
+  static void IRAM_ATTR touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data);
 };
 
 }  // namespace hd_device
